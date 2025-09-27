464f634790959cbf985c9854963a7e6d73465d3c
Mukul Pretham <mukulpretham@gmail.com>
2025-09-27 12:57:35 +0530

    cors added 2

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -13,6 +13,7 @@ app.use(cors())
 app.use(express.json());
 dotenv.config();
 
+
 // CORS middleware (must be before routes)
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
